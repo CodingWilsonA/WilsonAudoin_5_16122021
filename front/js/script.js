2d2a282e90516a430781c9716e03482a98a4d760
CodingWilsonA <wilson.audoin@gmail.com>
2022-01-24 17:46:08 +0100

    Factorized into smaller functions

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-//The function below displays required products img, h3 and p on the home page
+//The function below fetches products array
 function displayProducts() {
     fetch("http://localhost:3000/api/products")
         .then(function(res) {
@@ -8,48 +8,45 @@ function displayProducts() {
                 return res.json();
             }
         })
-        .then(function(value) {
-                for (let i in value) {
-                    const displayedProduct = document.getElementById("items");
-                    const displayedProductLink = document.createElement("a");
-                    const displayedProductArticle = document.createElement("article");
-                    const displayedProductImg = document.createElement("img");
-                    const displayedProductTitle = document.createElement("h3");
-                    const displayedProductDescription = document.createElement("p");
-                    displayedProductLink.setAttribute("href", "./product.html?id=" + value[i]._id);
-                    displayedProductImg.setAttribute("src", value[i].imageUrl);
-                    displayedProductImg.setAttribute("alt", value[i].altTxt);
-                    displayedProductTitle.classList.add("productName");
-                    displayedProductTitle.innerText = value[i].name;
-                    displayedProductDescription.classList.add("productDescription");
-                    displayedProductDescription.innerText = value[i].description;
-                    displayedProduct.appendChild(displayedProductLink);
-                    displayedProductLink.appendChild(displayedProductArticle);
-                    displayedProductArticle.appendChild(displayedProductImg);
-                    displayedProductArticle.appendChild(displayedProductTitle);
-                    displayedProductArticle.appendChild(displayedProductDescription);
-                }
-            })
-            .catch(function(err) {
-                console.error("The fetch request returned the following text :",err);
-                const displayedError = document.getElementById("items");
-                const displayedErrorLink = document.createElement("a");
-                const displayedErrorArticle = document.createElement("article");
-                const displayedErrorImg = document.createElement("img");
-                const displayedErrorTitle = document.createElement("h3");
-                const displayedErrorDescription = document.createElement("p");
-                displayedErrorLink.setAttribute("href", "#");
-                displayedErrorImg.setAttribute("src", "../images/icons/fetch-error.png" );
-                displayedErrorImg.setAttribute("alt", "Cette image représente un ouvrier de chantier. Elle est affichée en cas d'erreur de chargement de la page.");
-                displayedErrorTitle.classList.add("productName");
-                displayedErrorTitle.innerText = "Oops";
-                displayedErrorDescription.classList.add("productDescription");
-                displayedErrorDescription.innerText = "Nous travaillons à résoudre cette erreur.";
-                displayedError.appendChild(displayedErrorLink);
-                displayedErrorLink.appendChild(displayedErrorArticle);
-                displayedErrorArticle.appendChild(displayedErrorImg);
-                displayedErrorArticle.appendChild(displayedErrorTitle);
-                displayedErrorArticle.appendChild(displayedErrorDescription);
-    });
+        .then(function(displayAllProducts) {
+            setAllProducts(displayAllProducts);
+        })
+        .catch(function(err) {
+            console.error("The fetch request returned the following text :",err);
+            const errorObject = {
+               errorArray : {
+                _id : "#",
+                imageUrl : "../images/icons/fetch-error.png",
+                altTxt : "Cette image représente un ouvrier de chantier. Elle est affichée en cas d'erreur de chargement de la page.",
+                name : "Oops",
+                description : "Nous travaillons à résoudre cette erreur."
+               } 
+            };
+            setAllProducts(errorObject);
+        });
+}
+
+//The function below sets products' title, img and description
+function setAllProducts(displayAllProducts) {
+    for (let i in displayAllProducts) {
+        const displayedProduct = document.getElementById("items");
+        const displayedProductLink = document.createElement("a");
+        const displayedProductArticle = document.createElement("article");
+        const displayedProductImg = document.createElement("img");
+        const displayedProductTitle = document.createElement("h3");
+        const displayedProductDescription = document.createElement("p");
+        displayedProductLink.setAttribute("href", "./product.html?id=" + displayAllProducts[i]._id);
+        displayedProductImg.setAttribute("src", displayAllProducts[i].imageUrl);
+        displayedProductImg.setAttribute("alt", displayAllProducts[i].altTxt);
+        displayedProductTitle.classList.add("productName");
+        displayedProductTitle.innerText = displayAllProducts[i].name;
+        displayedProductDescription.classList.add("productDescription");
+        displayedProductDescription.innerText = displayAllProducts[i].description;
+        displayedProduct.appendChild(displayedProductLink);
+        displayedProductLink.appendChild(displayedProductArticle);
+        displayedProductArticle.appendChild(displayedProductImg);
+        displayedProductArticle.appendChild(displayedProductTitle);
+        displayedProductArticle.appendChild(displayedProductDescription);
+    }
 }
 displayProducts();
